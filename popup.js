7c62f7184fa41d1804e40e033933887e5bffa86a
github username <github email address>
2018-08-16 13:14:23 -0400

    sheets Id and Range stored

---
+++ popup.js
@@ -0,0 +1,21 @@
+$(function(){
+
+  let sheetsId = ''
+  let sheetsRange = ''
+
+  $('#gsheetsId').keyup(function(){
+    sheetsId = $(this).val()
+  })
+
+  $('#rowRange').keyup(function(){
+    sheetsRange = $(this).val()
+  })
+
+  $('#getSheetValue').click(function(){
+    chrome.storage.sync.set({'sheetsId':sheetsId});
+    chrome.storage.sync.set({'sheetsRange':sheetsRange});
+
+  })
+
+
+})
