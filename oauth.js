ad2459af5c4556899eda78964ed1caaa4d65c295
github username <github email address>
2018-08-20 18:37:07 -0400

    able to retrieve Sheetsdata

---
+++ oauth.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById('signin-button').addEventListener('click', function() {
+      chrome.identity.getAuthToken({interactive: true}, function(token) {
+      chrome.storage.sync.set({ 'token': token });
+      });
+    });
+  };
--- popup.js
+++ popup.js
@@ -12,9 +12,29 @@ $(function(){
   })
 
   $('#getSheetValue').click(function(){
-    chrome.storage.sync.set({'sheetsId':sheetsId});
-    chrome.storage.sync.set({'sheetsRange':sheetsRange});
-    alert("clicked ")
-  })
+    // chrome.storage.sync.set({'sheetsId':sheetsId});
+    // chrome.storage.sync.set({'sheetsRange':sheetsRange});
+    // alert(`${sheetsId}, ${sheetsRange}`)
 
+    chrome.storage.sync.get(['token'], function(auth){
+      console.log(auth.token);
+      let init = {
+        method: 'GET',
+        async: true,
+        headers: {
+          Authorization: 'Bearer ' + auth.token,
+          'Content-Type': 'application/json'
+        },
+        'contentType': 'json'
+      };
+      fetch(
+          `https://sheets.googleapis.com/v4/spreadsheets/${sheetsId}/values/${sheetsRange}`,
+          init)
+          .then((response) => response.json())
+          .then(function(data) {
+            console.log(data.values)
+            chrome.storage.sync.set({'sheetsValue': data.values})
+          });
+    })
+  })
 })
